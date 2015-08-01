7e3c8d7039b49ec8e5dafbbcc6f6de8cc860e644
Rodrigo Hurtado <rohulo@hotmail.es>
2015-07-31 22:57:16 -0500

    Primera pregunta del obligatorio

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,17 @@
+
+//interaccion de las peticiones del router
+//get question
+
+exports.question = function(req, res){
+	res.render('quizes/question', { pregunta: 'Capital de Italia' });
+};
+
+//get answer
+
+exports.answer = function(req, res){
+	if(req.query.respuesta === 'Roma'){
+		res.render('quizes/answer', { respuesta: "Correcto !"});
+	}else{
+		res.render('quizes/answer', { respuesta: "Incorrecto"});
+	}
+};
